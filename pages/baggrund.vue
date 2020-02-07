ed13bd93c012cd6d4cd5c8ba9e6d6343bd58273b
Mathias Hammer <>
2020-02-07 13:35:03 +0100

    Create baggrund.vue

---
+++ pages/baggrund.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="hero-image bg-hero">
+      <div>
+        <h1 class="title">Baggrund</h1>
+      </div>
+    </div>
+    <div>
+      <h2>Uddannelse</h2>
+      <h2>Erfaring</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style>
+</style>
