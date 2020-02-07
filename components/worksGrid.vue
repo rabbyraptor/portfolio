7876d83bf6985529585c45ff32b1a316bb845a59
Mathias Hammer <>
2020-02-07 13:34:58 +0100

    Create worksGrid.vue

---
+++ components/worksGrid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="works-grid">
+      <div v-for="work in works" class="work" :style="'background-color:' + work.color">
+          <h3>
+              {{ work.title }}
+          </h3>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            works:[
+                {
+                    title: "Work 1",
+                    color: "brown"
+                },
+                {
+                    title: "Work 2",
+                    color: "blue"
+                },
+                {
+                    title: "Work 3",
+                    color: "green"
+                },
+                {
+                    title: "Work 4",
+                    color: "yellow"
+                },
+                {
+                    title: "Work 5",
+                    color: "teal"
+                },
+                
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
